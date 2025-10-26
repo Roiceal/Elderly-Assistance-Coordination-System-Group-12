71917d5a1a64da48b3dd4c8bfdc74d079b6bb013
Aliyah Calanoc <liyahclnc26@gmail.com>
2025-10-26 18:20:20 +0800

    Update attendance_log.php

--- wdbconnection/attendance_log.php
+++ wdbconnection/attendance_log.php
@@ -1,16 +1,1 @@
-<?php
-header('Content-Type: application/json');
 
-$conn = new mysqli("localhost", "root", "", "sampleattendance");
-if ($conn->connect_error) { exit; }
-
-$sql = "SELECT * FROM attendance_records ORDER BY time_in DESC";
-$result = $conn->query($sql);
-$rows = [];
-while($row = $result->fetch_assoc()) {
-    $rows[] = $row;
-}
-echo json_encode($rows);
-
-$conn->close();
-?>
