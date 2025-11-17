3cfebd7134edd7828c554ee50b29960980048120
Aliyah Calanoc <liyahclnc26@gmail.com>
2025-11-17 18:46:24 +0800

    Add files via upload

---
+++ updated/database/db_connection.php
@@ -0,0 +1,39 @@
+<?php
+class Database {
+
+
+    private $db_server = "localhost";
+    private $db_user = "root";
+    private $db_pass = "";
+    private $db_name = "elderlyassistancecoordinationdb";
+    public $conn;
+
+    //private $db_server = "sql201.infinityfree.com";
+    //private $db_user = "if0_40425153";
+    //private $db_pass = "ARxaZZB53Zmmhb";
+    //private $db_name = "if0_40425153_elderlyassistancecoordinationdb";
+    //public $conn;
+    
+    public function __construct() {
+        $this->connect();
+    }
+
+    private function connect() {
+        $this->conn = new mysqli($this->db_server, $this->db_user, $this->db_pass, $this->db_name);
+
+        if ($this->conn->connect_error) {
+            die("Database connection failed: " . htmlspecialchars($this->conn->connect_error));
+        }
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+
+    public function close() {
+        if ($this->conn) {
+            $this->conn->close();
+        }
+    }
+}
+?>
